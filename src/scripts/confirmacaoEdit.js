a1734d5d00f6a95df93cb976f1a3b1db21022cf6
David-Silva2004 <162159885+David-Silva2004@users.noreply.github.com>
2024-05-27 19:33:06 -0300

    Function modal edit

--- src/scripts/confirmacaoEdit.js
+++ src/scripts/confirmacaoEdit.js
@@ -1,35 +1,35 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Obtém os elementos do DOM
-var modal = document.getElementById("confirmModal");
-var editButton = document.getElementById("editButton");
-var closeButton = document.getElementsByClassName("close")[0];
-var cancelButton = document.getElementById("cancelButton");
-var confirmEditButton = document.getElementById("confirmEditButton");
+    var modal = document.getElementById("confirmModal2");
+    var editButton = document.getElementById("editButton");
+    var span = document.getElementsByClassName("closeButton")[0];
+    var cancelButton = document.getElementById("cancelButton");
+    var confirmEditButton = document.getElementById("confirmEditButton");
 
-// Quando o usuário clicar no botão "Salvar", abre o modal
-editButton.onclick = function () {
-    modal.style.display = "block";
-}
+    // Quando o usuário clicar no botão "editar", abre o modal
+    editButton.onclick = function () {
+        modal.style.display = "block";
+    }
 
-// Quando o usuário clicar no (x), fecha o modal
-closeButton.onclick = function () {
-    modal.style.display = "none";
-}
-
-// Quando o usuário clicar no botão "Cancelar", fecha o modal
-cancelButton.onclick = function () {
-    modal.style.display = "none";
-}
-
-// Quando o usuário clicar no botão "Confirmar", envia o formulário
-confirmEditButton.onclick = function () {
-    document.getElementById("editForm").submit();
-}
-
-// Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
-window.onclick = function (event) {
-    if (event.target == modal) {
+    // Quando o usuário clicar no (x), fecha o modal
+    span.onclick = function () {
         modal.style.display = "none";
     }
-}
-});
+
+    // Quando o usuário clicar no botão "Cancelar", fecha o modal
+    cancelButton.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // Quando o usuário clicar no botão "Salvar", envia o formulário
+    confirmEditButton.onclick = function () {
+        document.getElementById("editForm").submit();
+    }
+
+    // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    });

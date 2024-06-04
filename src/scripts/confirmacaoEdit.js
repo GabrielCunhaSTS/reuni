6827d3ebc849d946ff07b3c2138c94c90508cead
David-Silva2004 <ds.davidsilva2004@gmail.com>
2024-06-04 08:09:03 -0300

    Editar Perfil

--- src/scripts/confirmacaoEdit.js
+++ src/scripts/confirmacaoEdit.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Obtém os elementos do DOM
-    var modal = document.getElementById("confirmModal2");
+    var modal = document.getElementById("confirmModalEdit");
     var editButton = document.getElementById("editButton");
     var span = document.getElementsByClassName("closeButton")[0];
-    var cancelButton = document.getElementById("cancelButton");
+    var cancelButton = document.getElementById("cancelButtonEdit");
     var confirmEditButton = document.getElementById("confirmEditButton");
 
     // Quando o usuário clicar no botão "editar", abre o modal
@@ -22,9 +22,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     // Quando o usuário clicar no botão "Salvar", envia o formulário
-    confirmEditButton.onclick = function () {
-        document.getElementById("editForm").submit();
-    }
+    document.getElementById('confirmEditButton').addEventListener('click', function() {
+        var nome = document.getElementById('nome').value;
+        var email = document.getElementById('email').value;
+        var descricao = document.getElementById('descricao').value;
+        var image = document.getElementById('profile').value
+        localStorage.setItem('nome', nome);
+        localStorage.setItem('email', email);
+        localStorage.setItem('descricao', descricao);
+        localStorage.setItem('profile', image)
+        // Redireciona para a página intermediária
+        // window.location.href = '/pagina que ele vai depois que apertar salvar';
+    });
 
     // Quando o usuário clicar em qualquer lugar fora do modal, fecha o modal
     window.onclick = function (event) {
